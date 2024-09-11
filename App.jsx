f2f209bbd49f6a845ec7891757d637ea5a27c097
nedjem <justmrpepe@gmail.com>
2024-09-11 18:18:16 +0100

    create react native app

---
+++ App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+function App(){
+  return (
+    <SafeAreaView style={styles.sectionContainer}>
+      <View>
+        <Text>hello react native</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+
+});
+
+export default App;
